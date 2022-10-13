6294c41bcc57085c679bf617689960a64788532c
siddhamruth <siddhamruth@gmail.com>
2022-10-14 01:09:36 +0530

    this is the starting of my  github journey in linux for C programming

---
+++ test1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("\n sample C program\n");
+	int data = 0;
+	printf("%d",data);
+	return 0;
+}
---
+++ test2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int age;
+	char name[100];
+	printf("What is your name?");
+	scanf("%s",&name[0]);
+	printf("What is your age?");
+	scanf("%d",&age);
+	printf("%s is born in %d \n",name, 2022-age);
+	
+}
